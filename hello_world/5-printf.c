bb730a80452b4045cde86737c8e4a63216029514
Super User <nezimk@outlook.fr>
2023-10-15 12:51:54 -0700

    Write c program with print f

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,13 +2,14 @@
 
 /**
  * main - prints "with proper grammar, but the outcome is a piece of art,"
- * Return: Always 0.
+ * Return: Maybe 0.
+ *
  */
 
 int main(void)
 
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
 }
