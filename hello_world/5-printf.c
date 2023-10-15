4d110eb498ce3e7af57af07d6b36d8e8e9529dfd
Super User <nezimk@outlook.fr>
2023-10-15 12:37:27 -0700

    Write c program with print f

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.>
 
+/**
+ * main - "with proper grammar, but the outcome is a piece of art,.
+ * Return: Always 0
+ */
+
 int main (void)
 {
 	printf("with proper grammar, but the outcome is a piece of art");
