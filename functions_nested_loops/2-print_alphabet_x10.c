1cca062c0eaedca8909d015d6c3f06b9d7ac290d
Super User <nezimk@outlook.fr>
2023-10-18 06:15:22 -0700

    create function that prints 10 times the alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,17 @@
  * Not return: 0.
  */
 
-void print_alphabet_x10(void);
-
+void print_alphabet_x10(void)
 {
 	char alphabet;
+	int i;
 
+	for (i = 0; i < 10; i++)
+	{
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		_putchar(alphabet, 10);
+		_putchar(alphabet);
 	}
-
 	_putchar('\n');
+	}
 }
