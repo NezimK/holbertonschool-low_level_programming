289105345a088e55e5ddc28d8766d4f40dc358ba
Super User <nezimk@outlook.fr>
2023-10-19 07:46:54 -0700

    check for alphabetic char

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c : check
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'a') || (c >= 'A' && c <= 'Z'))
+
+	return (1);
+
+	return (0);
+}
