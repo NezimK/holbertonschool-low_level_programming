65a959a82385a45f2ba17e2e56bff8892ce7ae82
Super User <nezimk@outlook.fr>
2023-10-22 12:41:52 -0700

    isuper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Check function
+ * @c: check
+ * Return: 0 or 1.
+ */
+
+int _isupper(int c)
+{
+	int _isupper(int c) 
+
+	if (isupper(c)) 
+	{
+	return (1);
+	}
+       	else 
+	{
+	return (0); 
+	}
+}
