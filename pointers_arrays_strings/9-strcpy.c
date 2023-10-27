8dc63af70d63f796a2c730b34332fc7ead44da52
Super User <nezimk@outlook.fr>
2023-10-27 07:16:42 -0700

    copie string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 
 /**
- * _strcpy : copie the string pointed to by src
- * @s: string
+ * _strcpy - copie the string pointed to by src
+ * @src: string source
+ * @dest: string out
  * Return: 0.
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+
 	for (i = 0; src[i] != '\0'; i++)
 	{
 	dest[i] = src[i];
