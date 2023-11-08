aa85a6a0ac534cd127bdecd87185d376441c03e7
Super User <nezimk@outlook.fr>
2023-11-08 06:46:26 -0800

    change error

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -33,8 +33,8 @@ int **alloc_grid(int width, int height)
 	}
 
 	for (i = 0; i < height; i++)
-	for (j = 0; j < width; j++)
-		array[i][j] = 0;
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
 
-		return (array);
+	return (array);
 }
