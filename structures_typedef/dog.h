c0a919a64658abdc007178bd7818df30e477269c
Super User <nezimk@outlook.fr>
2023-11-13 01:30:04 -0800

    create structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+#include <stdio.h>
+
+/**
+ * struct dog - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: elements.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
