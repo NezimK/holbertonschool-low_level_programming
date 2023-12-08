58a405724ad1d0be28c6d55c340ce6df628833a5
NezimK <alimekzine@hotmail.fr>
2023-12-08 18:18:01 +0100

    function implementing the djb2 algorithm.

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - create hash.
+ * @str: string
+ *
+ * Return: hash.
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 0;
+	int c = 0;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
